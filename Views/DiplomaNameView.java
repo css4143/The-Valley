afd334bf9dfafa3a91bbaf3b3e74d4c5f3ed209f
hollyhastings <hhastings111@gmail.com>
2017-09-17 09:25:36 -0400

    Add new diploma view to allow players to enter player name

---
+++ Views/DiplomaNameView.java
@@ -0,0 +1,24 @@
+package Views;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class DiplomaNameView extends GameMainView {
+    private JPanel diploma;
+    private JPanel paper;
+    private JLabel schoolName1;
+    private JLabel schoolName2;
+    private JTextField playerName;
+    private JLabel certification;
+    private JLabel metLine;
+    private JLabel bSLabel;
+    private JLabel president;
+    private JLabel dean;
+    private JLabel presidentTitle;
+    private JLabel deanTitle;
+    private JLabel trusteeTitle;
+    private JLabel trustee;
+    private JButton graduateButton;
+
+
+}
--- Views/GameMainView.java
+++ Views/GameMainView.java
@@ -4,17 +4,28 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GameMainView {
-    JPanel titlePageView = new JPanel();
-    JButton startButton = new JButton();
-    JLabel  gameTitle = new JLabel();
-    JTextArea subTitle = new JTextArea();
+    private JPanel titlePageView;
+    private JButton startButton;
+    private JLabel  gameTitle;
+    private JTextArea subTitle;
 
+    /**
     public GameMainView() {
+         JPanel titleScreentoReplace;
+
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                /** Something **/
+
             }
         });
+    }
+     **/
+    private class StartButtonClicked implements ActionListener {
+
+        @Override
+        public void actionPerformed (ActionEvent e) {
+
+        }
     }
 }
